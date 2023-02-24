8b1d3f537dba394863ed4d8a55594199e664ac1b
Cameron <meme34903@gmail.com>
2023-02-23 17:56:22 -0800

    slider finalized

--- src/Components/General/Slider.tsx
+++ src/Components/General/Slider.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import "./Slider.css";
 
 function Slider() {
-  return <input className="Slider" type="range" min="0" max="5" defaultValue="0" />;
+  return (
+    <div>
+      <input className="Slider" type="range" min="0" max="5" defaultValue="0" />
+    </div>
+  );
 }
 
 export default Slider;
